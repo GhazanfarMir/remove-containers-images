d449423c118f0463b55bb83637c125b8ab005ce1
Ghazanfar Mir <Ghazanfar.Mir@ebury.com>
2016-11-23 16:00:29 +0000

    Initial commit

---
+++ remove-images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+###########################################
+#
+# Simple Shell script to clean/remove all container/images
+#
+# The script will 
+#  - first stop all running containers (if any),
+#  - remove containers
+#  - remove images
+#
+
+# stop all running containers
+echo '####################################################'
+echo 'Stopping running containers (if available)...'
+echo '####################################################'
+docker stop $(docker ps -aq)
+
+# remove all stopped containers
+echo '####################################################'
+echo 'Removing containers ..'
+echo '####################################################'
+docker rm $(docker ps -aq)
+
+
+# remove all images
+echo '####################################################'
+echo 'Removing images ...'
+echo '####################################################'
+docker rmi $(docker images -q)
